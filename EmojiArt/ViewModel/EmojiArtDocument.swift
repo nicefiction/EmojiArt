a3b3a42c5318c59c5774c6ef50f8bdd93f2da03d
Olivier Van hamme <nicefiction@gmail.com>
2020-08-05 18:58:31 +0200

    Creates the EmojiArtDocument file .

---
+++ EmojiArt/ViewModel/EmojiArtDocument.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Olivier Van hamme on 05/08/2020.
+//  Copyright © 2020 nicefiction. All rights reserved.
+//
+
+import SwiftUI
+
+
+class EmojiArtDocument: ObservableObject {
+    
+    
+    
+} // class EmojiArtDocument: ObservableObject {}
