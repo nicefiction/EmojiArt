1b10843dec60fce1a0f3f6a6ee31fdd4d4711b07
Olivier Van hamme <nicefiction@gmail.com>
2020-08-05 18:49:46 +0200

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Olivier Van hamme on 05/08/2020.
+//  Copyright © 2020 nicefiction. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
